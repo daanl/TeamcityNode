323a3c3d9cf65ea190e54c83887570cf38374d2f
Daan Le Duc <daanleduc@hotmail.com>
2014-11-27 19:35:31 +0100

    initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name teamcityNodeApp
+ * @description
+ * # teamcityNodeApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('teamcityNodeApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name teamcityNodeApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the teamcityNodeApp
+ */
+angular.module('teamcityNodeApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
